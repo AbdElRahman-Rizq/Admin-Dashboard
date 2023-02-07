36efe898fac44da266741f30428e979d435b3e30
AbdElRahman Rizq <a.rezk453@gmail.com>
2023-02-07 05:28:46 +0200

    Added Profile-form section

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import{Route, Routes}from "react-router-dom";
 import Dashboard from "./scenes/dashboard";
 import Team from "./scenes/team/index.jsx";
 import Contacts from "./scenes/contacts/index.jsx";
+import Form from "./scenes/form/index";
 import Invoices from "./scenes/invoices/index";
 
 
@@ -23,12 +24,12 @@ function App() {
       <SideBar />
           <div className="content">
             <Topbar />
-            
             <Routes>
               <Route path="/" element={<Dashboard />}/>
               <Route path="Team" element={<Team />}/>
               <Route path="Contacts" element={<Contacts />}/>
               <Route path="Invoices" element={<Invoices />}/>
+              <Route path="Form" element={<Form/>}/>
             </Routes>
           </div>
     </div>
--- src/scenes/form/index.jsx
+++ src/scenes/form/index.jsx
@@ -1,8 +1,141 @@
-const Sidebar=()=>{
+import { Box, TextField } from "@mui/material";
+import { Formik } from "formik";
+import * as yup from "yup";
+import Header from "../../components/Header";
+const Form=()=>{
+    const handleFormSubmit=(values)=>{
+        console.log(values);
+    }
     return(        
-        <div>
-        Sidebar
-    </div>
+      <Box>
+      <Header title="CREATE USER" subtitle="Create a New User Profile" />
+        <Formik
+        initialValues={initialValues}
+        onSubmit={handleFormSubmit}
+        validationSchema={checkoutSchema}
+        >{
+            ({
+                  values,
+                  errors,
+                  touched,
+                  handleChange,
+                  handleBlur,
+                  handleSubmit,
+                  isSubmitting,
+                  /* and other goodies */
+               }) => (
+            <form onSubmit={handleFormSubmit}>
+                <Box>
+                <TextField
+                fullWidth
+                variant="filled"
+                type="text"
+                label="First Name"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.firstName}
+                name="firstName"
+                error={!!touched.lastName && !!errors.lastName}
+                helperText={touched.firstName && errors.firstName}              
+              />
+                 <TextField
+                fullWidth
+                variant="filled"
+                type="text"
+                label="Last Name"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.lastName}
+                name="lastName"
+                error={!!touched.lastName && !!errors.lastName}
+                helperText={touched.lastName && errors.lastName}
+                sx={{ gridColumn: "span 2" }}
+              />
+              <TextField
+                fullWidth
+                variant="filled"
+                type="text"
+                label="Email"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.email}
+                name="email"
+                error={!!touched.email && !!errors.email}
+                helperText={touched.email && errors.email}                
+              />
+              <TextField
+                fullWidth
+                variant="filled"
+                type="text"
+                label="Contact Number"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.contact}
+                name="contact"
+                error={!!touched.contact && !!errors.contact}
+                helperText={touched.contact && errors.contact}                
+              />
+              <TextField
+                fullWidth
+                variant="filled"
+                type="text"
+                label="Address 1"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.address1}
+                name="address1"
+                error={!!touched.address1 && !!errors.address1}
+                helperText={touched.address1 && errors.address1}                
+              />
+              <TextField
+                fullWidth
+                variant="filled"
+                type="text"
+                label="Address 2"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.address2}
+                name="address2"
+                error={!!touched.address2 && !!errors.address2}
+                helperText={touched.address2 && errors.address2}                
+              />
+                </Box>
+
+                <Box sx={{
+                    "& Button":{
+                        backgroundColor : "#4cceac;",
+                    }
+                }}>
+                <button type="submit" >
+                Create New User
+              </button>
+                </Box>
+            </form>)}
+        </Formik>
+      </Box>
         )
-}
-export default Sidebar;
+    }
+    
+const phoneRegExp =
+/^((\+[1-9]{1,4}[ -]?)|(\([0-9]{2,3}\)[ -]?)|([0-9]{2,4})[ -]?)*?[0-9]{3,4}[ -]?[0-9]{3,4}$/;
+
+const checkoutSchema = yup.object().shape({
+    firstName: yup.string().required("required"),
+    lastName: yup.string().required("required"),
+    email: yup.string().email("invalid email").required("required"),
+    contact: yup
+    .string()
+    .matches(phoneRegExp, "Phone number is not valid")
+    .required("required"),
+  address1: yup.string().required("required"),
+  address2: yup.string().required("required"),
+})
+        const initialValues={
+            firstName: "",
+            lastName: "",
+            email: "",
+            contact: "",
+            address1: "",
+            address2: "",
+        }
+export default Form;
